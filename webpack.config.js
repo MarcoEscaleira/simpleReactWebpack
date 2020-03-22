53f20f5d8db46ac7bd7b404e506903b42c519f33
Marco Escaleira <marcoescaleira2000@gmail.com>
2020-03-22 23:18:31 +0000

    fix: restoring deleted file

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist/"
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [ "@babel/env" ],
+            plugins: [ "transform-class-properties" ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new TerserPlugin()
+  ]
+};

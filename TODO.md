317f3a3e018f83d54679260ffc1df932c4370f44
Marco Escaleira <marcoescaleira2000@gmail.com>
2020-03-29 00:01:04 +0000

    refactor: cleaning code and simplifying

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+ 1. Source map eval
+ 2. Check fonts being dynamic
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import HelloWorld from './components/helloWorldButton/helloWorldButton';
-import Heading from './components/heading/heading';
-
-new Heading().render();
-new HelloWorld().render(); 
-
 if (process.env.NODE_ENV === "production") {
   console.log("production mode");
 }
